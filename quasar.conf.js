260047cce908872d3320b14313a8d7e44efe3bcc
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-12-22 18:52:58 +0900

    minify false

--- quasar.conf.js
+++ quasar.conf.js
@@ -60,7 +60,7 @@ module.exports = function (/* ctx */) {
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
-
+      minify: false,
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
         minify: false
